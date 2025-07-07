3c978efc29118b06c84cb6785f60c449e996e55d
Ernesto González <ing.ernestogonzalezr@gmail.com>
2025-07-07 17:16:32 -0600

    corregido

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,17 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
 const $n = document.querySelector('.name');
-const $b = document.querySelector('#blog');
+const $b = document.querySelector('.blog'); //cambié # a . ya que en el html es clase
 const $l = document.querySelector('.location');
 
-async function displayUser(username) {
+async function displayUser(username) { //await debe ir dentro de una función async
   $n.textContent = 'cargando...';
   try {
     const response = await fetch(`${usersEndpoint}/${username}`);
-    const data = await response.json();
+    const data = await response.json(); //no se estaba usando el .json
     console.log(data);
 
-    $n.textContent = `${data.name}` ;
+    $n.textContent = `${data.name}` ; //deben ir entre backtics
     $b.textContent = `${data.blog}`;
     $l.textContent = `${data.location}`;
   } catch (err) {
@@ -22,7 +22,7 @@ async function displayUser(username) {
 function handleError(err) {
     console.log('OH NO!');
     console.log(err);
-    $n.textContent = `Algo salió mal: ${err}`
+    $n.textContent = `Algo salió mal: ${err}` //la variable n no existe, es $n
   }
 
   displayUser('stolinski').catch(handleError);
